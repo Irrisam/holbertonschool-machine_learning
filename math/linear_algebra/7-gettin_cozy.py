acf46d44b6e97327d4ef03b0a966632be7494c89
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-12-06 00:11:34 +0100

    add check for rows and columns count

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,7 +4,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """function that concatenates along specified axis"""
-    if len(mat1) != len(mat2):
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
+        """checks for same number of columns"""
+        return None
+    elif axis == 1 and len(mat1) != len(mat2):
+        """checks for same number of rows"""
         return None
     result = []
     if axis == 0:
