49ef6626480487d526a20ab4aa43b829540cd458
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-12-06 17:36:26 +0100

    remove loops and simplify (alot)

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -4,25 +4,4 @@
 
 def np_elementwise(mat1, mat2):
     """function that returns operations on matrices"""
-    if mat1 is not int or mat2 is not int:
-        if len(mat1) != len(mat2):
-            """checks for shape"""
-            return None
-        for row in mat2:
-            if len(row) != len(mat1[0]):
-                return None
-
-        sum_matrix = [[] for _ in range(len(mat1))]
-        diff_matrix = [[] for _ in range(len(mat1))]
-        mul_matrix = [[] for _ in range(len(mat1))]
-        div_matrix = [[] for _ in range(len(mat1))]
-
-        for counter, (i, j) in enumerate(zip(mat1, mat2)):
-            """adds a counter in order to insert new values in new matrice"""
-            for x, y in zip(i, j):
-                """digs within each new tuple to get new values"""
-                sum_matrix[counter].append(x + y)
-                diff_matrix[counter].append(x - y)
-                mul_matrix[counter].append(x * y)
-                div_matrix[counter].append(x / y)
-        return sum_matrix, diff_matrix, mul_matrix, div_matrix
+    return (mat1+mat2, mat1 - mat2, mat1*mat2, mat1/mat2)
