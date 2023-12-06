961d499f480570a90824e14eae8dfb53a2242e82
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-12-06 11:07:44 +0100

    add task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function that transposes a function using np"""
+
+
+def np_transpose(matrix):
+    """function that transposes a function using np"""
+    return matrix.transpose
