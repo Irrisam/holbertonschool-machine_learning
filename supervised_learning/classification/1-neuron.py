3e1c26506fc1a2c1a7e06b64a2d57b964d79c3c2
Irrisam <124586557+Irrisam@users.noreply.github.com>
2024-01-22 14:08:31 +0100

    add task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""neuron file from classification project"""
+import numpy as np
+
+
+class Neuron:
+    """Class building for neurons"""
+    def __init__(self, nx):
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        else:
+            self.nx = nx
+            self.__W = np.random.normal(loc=0.0, scale=1.0, size=(1, nx))
+            self.__b = 0
+            self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
