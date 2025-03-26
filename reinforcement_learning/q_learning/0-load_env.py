651974628b000a1b79ca8dff14109a17af28aa34
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-03-26 10:46:21 +0100

    add 0

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-    Module to load environment frozen-lake from gym
+    module to load the frozen-lake environment from gym
 """
 import gym
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
-        load the premade FrozenLakeEnv env from OpenAI's gym
+        loads the pre-built FrozenLakeEnv environment from OpenAI's gym
 
-        :param desc: None or list custom description of the map to load
-        :param map_name: None or string pre-made map to load
-        :param is_slippery: bool, determine if the ice is slippery
+        :param desc: None or list, custom description of the map (optional)
+        :param map_name: None or string, choose a pre-made map (optional)
+        :param is_slippery: bool, defines if the ice is slippery or not
 
-        :return: the env
+        :return: the environment object
     """
     env = gym.make('FrozenLake-v0',
                    desc=desc,
