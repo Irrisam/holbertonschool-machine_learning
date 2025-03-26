7c8eef734a151f9238bcb5cfbee4136593b63a6a
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-03-26 10:56:38 +0100

    update frozen lake version

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -15,7 +15,7 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
 
         :return: the environment object
     """
-    env = gym.make('FrozenLake-v0',
+    env = gym.make('FrozenLake-v1',
                    desc=desc,
                    map_name=map_name,
                    is_slippery=is_slippery)
