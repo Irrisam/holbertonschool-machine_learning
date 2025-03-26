d1b88478d4fe2ef83b9e3314d325f6b7897af56c
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-03-26 10:31:18 +0100

    remove undone project about nlp and add first task of qa bot

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    Module Question Answering
+    Module Query Response
 """
 
 import tensorflow as tf
@@ -8,46 +8,46 @@ import tensorflow_hub as hub
 from transformers import BertTokenizer
 
 
-def question_answer(question, reference):
+def retrieve_answer(query, context):
     """
-        finds a snippet of text within a reference document
-        to answer a question
+        Identifies a segment of text within a given reference
+        to respond to a posed query
 
-    :param question: string, question to answer
-    :param reference: string, reference document from which find answer
+    :param query: string, inquiry needing an answer
+    :param context: string, supporting document to locate response
 
-    :return: string, answer
-        if no answer: None
+    :return: string, extracted answer
+        if no response found: None
     """
 
-    # load tokenizer & model
+    # initialize tokenizer & model
     tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word'
                                               '-masking-finetuned-squad')
     model = (
         hub.load("https://www.kaggle.com/models/seesee/bert/"
                  "TensorFlow2/uncased-tf2-qa/1"))
 
-    # tokenize inputs
-    q_tokenized = tokenizer.tokenize(question)
-    ref_tokenized = tokenizer.tokenize(reference)
+    # tokenize question and reference
+    q_tokens = tokenizer.tokenize(query)
+    ctx_tokens = tokenizer.tokenize(context)
 
     ###########################
-    # PREPROCESS INPUT TOKENS #
+    #   TOKEN PREPROCESSING   #
     ###########################
 
-    # concatenate token with special tokens
-    tokens = ['[CLS]'] + q_tokenized + ['[SEP]'] + ref_tokenized + ['[SEP]']
+    # merge tokens with special indicators
+    token_sequence = ['[CLS]'] + q_tokens + ['[SEP]'] + ctx_tokens + ['[SEP]']
 
-    # conversion in numerical ids
-    input_word_ids = tokenizer.convert_tokens_to_ids(tokens)
+    # transform tokens into numerical identifiers
+    input_word_ids = tokenizer.convert_tokens_to_ids(token_sequence)
 
-    # create tensor mask : 1 to consider this token
+    # generate tensor mask: 1 signifies relevant token
     input_mask = [1] * len(input_word_ids)
-    # mask to separate question - reference
-    input_type_ids = ([0] * (1 + len(q_tokenized) + 1) +
-                      [1] * (len(ref_tokenized) + 1))
+    # differentiate query from context
+    input_type_ids = ([0] * (1 + len(q_tokens) + 1) +
+                      [1] * (len(ctx_tokens) + 1))
 
-    # conversion in TensorFlow tensor
+    # transform into TensorFlow tensors
     input_word_ids, input_mask, input_type_ids = (
         map(lambda t: tf.expand_dims(
             tf.convert_to_tensor(t,
@@ -55,28 +55,28 @@ def question_answer(question, reference):
             (input_word_ids, input_mask, input_type_ids)))
 
     ###########################
-    #   INFERENCE WITH BERT   #
+    #  BERT MODEL INFERENCE   #
     ###########################
 
-    # pass in the model
-    outputs = model([input_word_ids, input_mask, input_type_ids])
-    # outputs = proba of each token to be start and end
-    # of the answer in given doc
+    # execute model inference
+    predictions = model([input_word_ids, input_mask, input_type_ids])
+    # predictions indicate probabilities of tokens being start & end
+    # of the extracted answer in provided text
 
     ###########################
-    #    EXTRACTION ANSWER    #
+    #    RESPONSE EXTRACTION   #
     ###########################
 
-    # found the most probably start token (+1 because ignore CLS)
-    short_start = tf.argmax(outputs[0][0][1:]) + 1
-    # found most probably end token (+1 because ignore CLS)
-    short_end = tf.argmax(outputs[1][0][1:]) + 1
-    # extraction of token corresponding to answer
-    answer_tokens = tokens[short_start: short_end + 1]
-    if not answer_tokens:
+    # determine most probable starting token (+1 to skip CLS token)
+    start_idx = tf.argmax(predictions[0][0][1:]) + 1
+    # determine most probable ending token (+1 to skip CLS token)
+    end_idx = tf.argmax(predictions[1][0][1:]) + 1
+    # retrieve token subset corresponding to answer
+    extracted_tokens = token_sequence[start_idx: end_idx + 1]
+    if not extracted_tokens:
         return None
 
-    # conversion answer token in understandable string
-    answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    # convert extracted tokens into readable text
+    response_text = tokenizer.convert_tokens_to_string(extracted_tokens)
 
-    return answer
+    return response_text
