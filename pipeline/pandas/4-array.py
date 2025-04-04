5b62d3bb2855210d1d26b47f400624c18190e0ee
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-04-04 17:01:27 +0200

    add 4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Converts a numpy array to a pandas DataFrame
+"""
+
+import pandas as pd
+import numpy as np
+
+
+def array(df):
+    """
+        Converts a numpy array to a pandas DataFrame.
+
+        :param df: pandas.DataFrame
+            The DataFrame to convert.
+
+        :return: numpy.ndarray
+            The converted numpy array.
+    """
+
+    arrayed_tails = df[['High', 'Close']].tail(10)
+
+    arrayed_tails = arrayed_tails.to_numpy()
+    return (arrayed_tails)
