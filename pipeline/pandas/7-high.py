5b94edd9c66cb2160e2e7a9be8e1e07bc302a5f1
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-04-04 17:33:59 +0200

    add 7

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Converts a numpy array to a pandas DataFrame
+"""
+
+
+import pandas as pd
+
+
+def high(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Sorts the DataFrame by the High price in descending order.
+
+    Parameters:
+    df (pd.DataFrame): Input DataFrame with a 'High' column.
+
+    Returns:
+    pd.DataFrame: Sorted DataFrame.
+    """
+    return df.sort_values(by='High', ascending=False)
