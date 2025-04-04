b66ba1c943037102b85debc23b3501c11628371c
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-04-04 16:39:12 +0200

    add 1

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Converts a numpy array to a pandas DataFrame
+"""
+import pandas as pd
+
+
+def dict_df_builder():
+    """
+        Converts a dictionary to a pandas DataFrame.
+
+        :return: pandas.DataFrame
+            The converted DataFrame.
+    """
+    dict_df = pd.DataFrame({'First': [0.0, 0.5, 1.0, 1.5], 'Second': ["one", "two", "three", "four"]})
+
+    return dict_df
+
+df = dict_df_builder()
