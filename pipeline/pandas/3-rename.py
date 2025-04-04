99e35c4dbee19f6a1dc9b3d1730e589133272e51
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-04-04 16:48:49 +0200

    add 3

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Converts a numpy array to a pandas DataFrame
+"""
+
+import pandas as pd
+
+def rename(df):
+    """
+        Renames the columns of a DataFrame.
+
+        :param df: pandas.DataFrame
+            The DataFrame to rename.
+
+        :return: pandas.DataFrame
+            The renamed DataFrame.
+    """
+    df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
+
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+
+    df = df[['Datetime', 'Close']]
+
+    return df
