c7f556dd3b610ef9c314dbfb7cbb997cab7f7fdb
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-19 15:27:23 +0200

    rename row

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT ts.title, tg.id
+SELECT ts.title, tg.id genre_id
 from
     tv_show_genres tsg
     JOIN tv_shows ts on ts.id = tsg.show_id
