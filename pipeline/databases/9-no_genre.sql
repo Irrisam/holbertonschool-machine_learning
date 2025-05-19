f74ee5bc66c25f0c7ef4d2acf5c0947a0ade17ff
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-19 15:23:53 +0200

    add 9

---
+++ pipeline/databases/9-no_genre.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT ts.title, tsg.genre_id
+FROM
+    tv_show_genres tsg
+    RIGHT JOIN tv_shows ts on ts.id = tsg.show_id
+WHERE
+    tsg.genre_id is NULL;
