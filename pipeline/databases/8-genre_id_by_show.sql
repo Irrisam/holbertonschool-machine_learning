7f378f2cf6ea745434a01b4f13751e5fb9b6674d
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-19 15:05:25 +0200

    add 8

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT ts.title, tg.id
+from
+    tv_show_genres tsg
+    JOIN tv_shows ts on ts.id = tsg.show_id
+    JOIN tv_genres tg on tg.id = tsg.genre_id
+ORDER BY ts.title, tg.id ASC;
