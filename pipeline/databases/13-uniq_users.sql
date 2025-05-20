2bf8fa5c21ca0a4d639cad67315e8b5579c010b9
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-20 09:34:28 +0200

    add 13

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(256),
+    name varchar(256),
+    PRIMARY KEY (id)
+);
