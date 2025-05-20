1d65253919db428f5131e3b3213a66d0051041bf
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-20 10:11:00 +0200

    add unique to emails for 13

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -2,7 +2,7 @@
 -- because Batch 3 is the best!
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
-    email varchar(256),
+    email UNIQUE varchar(256),
     name varchar(256),
     PRIMARY KEY (id)
 );
