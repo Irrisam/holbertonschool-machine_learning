7b7af0aba24238608a4d23bcb90dd9fac34908ce
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-20 11:09:30 +0200

    add 18

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,16 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+DELIMITER $$
+
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER;
+
+$$
