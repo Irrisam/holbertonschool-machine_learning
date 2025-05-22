4809e37c422910d33b52d428da44178651e7c384
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-05-22 09:58:59 +0200

    rectify 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     nginx_collection = client.logs.nginx
     
     # Total number of logs
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
+    total_logs = nginx_collection.count({})
+    print("{} logs".format(total_logs))
     
     # Methods stats
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count({"method": method})
+        print("\tmethod {}: {}".format(method, count))
     
     # GET requests to /status
-    status_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
+    status_count = nginx_collection.count({"method": "GET", "path": "/status"})
+    print("{} status check".format(status_count))
