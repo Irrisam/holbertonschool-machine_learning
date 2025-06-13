4a483db9548a374a36fb96cb705cd2d9b998b9d1
Tristan Duchamp <tristanduchamp@hotmail.fr>
2025-06-13 11:29:18 +0200

    remove custom main

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -4,7 +4,6 @@ contrasts an image
 """
 
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
 
 def change_hue(image, delta):
@@ -12,23 +11,3 @@ def change_hue(image, delta):
     randomly adjusts the contrast
     """
     return tf.image.adjust_hue(image, delta)
-
-
-def main():
-    # Load the image
-    image_path = "Original_Doge_meme.jpg"
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_image(image, channels=3)
-
-    # Rotate the image
-    rotated_image = change_hue(image, -0.5)
-
-    # Display the rotated image
-    plt.figure(figsize=(8, 8))
-    plt.imshow(rotated_image)
-    plt.axis('off')
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
